dba6544df7dc9f0496df0beb68b28c36c1a15fcf
Diego Freniche Brito <diego@freniche.com>
2015-09-22 09:50:35 +0200

    Added Notebook class

---
+++ app/src/main/java/io/keepcoding/everpobre/model/Notebook.java
@@ -0,0 +1,46 @@
+package io.keepcoding.everpobre.model;
+
+import java.util.Date;
+
+public class Notebook {
+    private long id;
+    private String name;
+    private Date creationDate;
+    private Date modificationDate;
+
+    public Notebook(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+}
