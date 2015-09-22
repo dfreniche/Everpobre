8190ba43beb87eec40c995dc01ff0a148afbce9c
Diego Freniche Brito <diego@freniche.com>
2015-09-22 08:00:21 +0200

    Launching EditNotebookActivity from main_menu

--- app/src/main/java/io/keepcoding/everpobre/activities/MainActivity.java
+++ app/src/main/java/io/keepcoding/everpobre/activities/MainActivity.java
@@ -1,7 +1,8 @@
 package io.keepcoding.everpobre.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,20 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_main_edit_notebook_action) {
+            Intent i = new Intent(this, EditNotebookActivity.class);
+            startActivity(i);
+
             return true;
         }
 
