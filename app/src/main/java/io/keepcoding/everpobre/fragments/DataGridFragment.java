ff7ddea96fb43cfeecd2378f949594fef7d13fee
Diego Freniche Brito <diego@freniche.com>
2015-09-24 18:12:22 +0200

    Added listener to DataGridFragment to make it reusable

--- app/src/main/java/io/keepcoding/everpobre/fragments/DataGridFragment.java
+++ app/src/main/java/io/keepcoding/everpobre/fragments/DataGridFragment.java
@@ -1,19 +1,28 @@
 package io.keepcoding.everpobre.fragments;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 import io.keepcoding.everpobre.R;
+import io.keepcoding.everpobre.adapters.DataGridAdapter;
 
 public class DataGridFragment extends Fragment {
+    public interface OnDataGridFragmentClickListener {
+        public void dataGridElementClick(AdapterView<?> parent, View view, int position, long id);
+        public boolean dataGridElementLongClick(AdapterView<?> parent, View view, int position, long id);
+    }
 
+    private OnDataGridFragmentClickListener listener;
+    private DataGridAdapter adapter;
+    private GridView gridNotebooks;
 
     public DataGridFragment() {}
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,4 +31,41 @@ public class DataGridFragment extends Fragment {
     }
 
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        gridNotebooks = (GridView)getActivity().findViewById(R.id.grid_notebooks);
+
+        gridNotebooks.setAdapter(adapter);
+
+        //Set Long-Clickable
+        gridNotebooks.setLongClickable(true);
+        gridNotebooks.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                return listener.dataGridElementLongClick(parent, view, position, id);
+            }
+        });
+
+        gridNotebooks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                listener.dataGridElementClick(parent, view, position, id);
+
+            }
+        });
+
+    }
+
+    public void setAdapter(DataGridAdapter adapter) {
+        this.adapter = adapter;
+    }
+
+    public void setOnDataGridFragmentListener(OnDataGridFragmentClickListener listener) {
+        if (listener == null) {
+            return;
+        }
+        this.listener = listener;
+    }
 }
